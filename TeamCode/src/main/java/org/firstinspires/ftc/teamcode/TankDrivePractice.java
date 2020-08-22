aeb80a56363328d6f76e5ebc64065453a1587461
drss.ftc.12653 <drss.ftc.12653@gmail.com>
2020-08-22 11:29:19 -0400

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrivePractice.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+@TeleOp(name = "TankDrivePractice", group = "team")
+
+public class TankDrivePractice extends OpMode {
+    DcMotor TopLeft;
+    DcMotor TopRight;
+    DcMotor BackLeft;
+    DcMotor BackRight;
+
+    double drivepower = 1;
+    double Zero;
+    double One;
+    double Two;
+    double Three;
+    double TopLeftPower;
+    double TopRightPower;
+    double BackLeftPower;
+    double BackRightPower;
+
+    public void init () {
+
+        TopLeft = hardwareMap.dcMotor.get ("TL");
+        TopLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        TopRight = hardwareMap.dcMotor.get("TR");
+        TopRight.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        BackLeft = hardwareMap.dcMotor.get("BL");
+        BackLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        BackRight = hardwareMap.dcMotor.get("BR");
+        BackRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        
+    }
+
+    public void loop () {
+
+        TopLeftPower = Range.clip(Math.pow(gamepad1.left_stick_y, 3), -drivepower, drivepower);
+        TopRightPower = Range.clip(Math.pow(gamepad1.right_stick_y, 3), -drivepower, drivepower);
+        BackLeftPower = Range.clip(Math.pow(gamepad1.left_stick_y, 3), -drivepower, drivepower);
+        BackRightPower = Range.clip(Math.pow(gamepad1.right_stick_y, 3), -drivepower, drivepower);
+
+        TopLeft.setPower(TopLeftPower);
+        TopRight.setPower(TopRightPower);
+        BackLeft.setPower(BackLeftPower);
+        BackRight.setPower(BackRightPower);
+
+    }
+}
